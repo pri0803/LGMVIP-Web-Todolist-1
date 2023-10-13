ff99a001b85de912c6c4c2c6d40f0b27ae05076b
Priya Baranwal <115931745+pri0803@users.noreply.github.com>
2023-10-13 19:16:57 +0530

    Created final App

---
+++ src/components/TodoForm.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+export default function TodoForm(props) {
+  const [input, setInput] = useState("");
+  const handleChange= e=> {
+    setInput(e.target.value)
+  }
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addTask({
+        id: Math.floor(Math.random()* 1000000),
+        text: input,
+        isComplete: false
+    })
+    setInput('')
+}
+
+  return (
+    <form className="todo-form" >
+      <input type="text"  placeholder="Add your task" onChange={handleChange} className="todo-input" value={input} name="text"/>
+      <button type="submit" onClick={handleSubmit} className="todo-btn">Add list</button>
+    </form>
+  );
+}
